e016df41067cb34ab9307f217efb26c790599ab7
USERXX <USERXXX@USERXXX-PC>
2018-08-18 13:24:27 +0300

    Project Created stage 2

---
+++ WoodMe/WoodMe/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WoodMe.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public int HomeNumber { get; set; }
+    }
+}
---
+++ WoodMe/WoodMe/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WoodMe.AdditionalClasses;
+
+namespace WoodMe.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+        public Texture Texture { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
