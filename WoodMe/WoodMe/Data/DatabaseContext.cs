24423e088a7333737dd536944626afe2b88fd72e
Maayan101 <maayan101@gmail.com>
2018-08-18 13:46:19 +0300

    DataBase Context

---
+++ WoodMe/WoodMe/Data/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WoodMe.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext (DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WoodMe.Models.Product> Product { get; set; }
+    }
+}
--- WoodMe/WoodMe/Startup.cs
+++ WoodMe/WoodMe/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WoodMe.Models;
 
 namespace WoodMe
 {
@@ -33,6 +35,9 @@ namespace WoodMe
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<DatabaseContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DatabaseContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
